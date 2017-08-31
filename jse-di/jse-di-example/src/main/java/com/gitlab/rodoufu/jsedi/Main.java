50c8a3fe89a2b9475a41f4e176092ecf9e563159
rodolfo <rodoufu@gmail.com>
2017-08-30 22:05:29 -0300

    Projetos

---
+++ jse-di/jse-di-example/src/main/java/com/gitlab/rodoufu/jsedi/Main.java
@@ -0,0 +1,14 @@
+package com.gitlab.rodoufu.jsedi;
+
+import com.gitlab.rodoufu.jsedi.cdi.WeldContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		WeldContext.INSTANCE.getBean(Main.class).execute();
+	}
+
+	private void execute() {
+		System.out.println("Funcionou");
+	}
+}
