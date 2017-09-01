8891cf8b9c25d571865e955253b6d46438ebf357
rodolfo <rodoufu@gmail.com>
2017-08-31 22:53:40 -0300

    Tentando fazer archetype

---
+++ jse-di/jse-di-archetype/src/main/resources/archetype-resources/src/main/java/WeldContext.java
@@ -0,0 +1,21 @@
+package $pom.cdi;
+
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+
+public class WeldContext {
+	public static final WeldContext INSTANCE = new WeldContext();
+
+	private final Weld weld;
+	private final WeldContainer container;
+
+	private WeldContext() {
+		weld = new Weld();
+		container = weld.initialize();
+	}
+
+	public <T> T getBean(Class<T> type) {
+		return container.select(type).get();
+	}
+
+}
